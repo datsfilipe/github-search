094841b7bd5051b7d8ba0388626243ee57984b0a
datsfilipe <datsfilipe.foss@proton.me>
2023-05-12 14:27:29 -0300

    chore: add vite + vite plugins

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,18 @@
 import { defineConfig } from 'vite'
 import { resolve } from 'path'
 import react from '@vitejs/plugin-react-swc'
+import ssr from 'vite-plugin-ssr/plugin'
+import UnoCSS from 'unocss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
+import { presetWind } from 'unocss'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    ssr(),
+    UnoCSS({ presets: [presetWind()] }),
+    VitePWA({ registerType: 'autoUpdate' }),
   ],
   resolve: {
     alias: {
