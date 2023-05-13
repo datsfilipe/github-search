e2401fc82a106aeb8696446194ee25c87f1e4b5e
datsfilipe <datsfilipe.foss@proton.me>
2023-05-13 14:39:31 -0300

    feat: add function and icon to search button

--- src/pages/index/index.page.tsx
+++ src/pages/index/index.page.tsx
@@ -1,6 +1,8 @@
 import { type ChangeEvent, useState, useEffect } from 'react'
 import { useSearch } from '@/hooks/useSearch'
 import { ResultsContainer } from '@/components/resultsContainer'
+import ghLogo from '@/assets/github.svg'
+import { toast } from 'react-hot-toast'
 
 type Result = Omit<User, 'login'> & {
   name: string
@@ -28,8 +30,17 @@ export const Page = () => {
     <div className={results.length > 0 ? 'w-full -translate-y-40 transition-transform duration-200 ease-in-out' : 'w-full transition-transform duration-200 ease-in-out'}>
       <h2 className="mx-auto w-fit font-bold text-6xl mb-10">GitHub Search</h2>
       <div className="flex items-center rounded-lg bg-zinc-200 max-w-3xl sm:max-w-4/6 mx-auto overflow-hidden">
-        <button className="px-4 py-2 bg-zinc-300 hover:bg-zinc-400 transition-colors duration-300 ease-in-out text-neutral-900">
-          Search
+        <button
+          className="text-xl px-4 py-2 bg-zinc-300 hover:bg-zinc-400 transition-colors duration-300 ease-in-out text-neutral-900"
+          onClick={() => { 
+            if (search === '') toast.error('Please enter a search term')
+
+            handleSearch(search).catch((error) => {
+              console.error(error)
+            })
+          }}
+        >
+          <img src={ghLogo} alt="GitHub Logo" className="w-6 h-6 inline-block text-neutral-300" />
         </button>
         <input
           className="bg-transparent w-full p-2 text-neutral-900"
