1680614b9e667740ebd5bffa187565d1528a48e5
datsfilipe <datsfilipe.foss@proton.me>
2023-05-12 22:36:09 -0300

    refactor: move global components and styles to Layout

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { PageContextProvider } from '@/renderer/usePageContext'
 import type { PageContext } from '@/renderer/types'
+import { Toaster } from 'react-hot-toast'
 import '@unocss/reset/tailwind.css'
 import 'virtual:uno.css'
 import './global.css'
@@ -11,7 +12,10 @@ function Layout({ children, pageContext }: { children: React.ReactNode; pageCont
   return (
     <React.StrictMode>
       <PageContextProvider pageContext={pageContext}>
-        {children}
+        <main className="flex flex-col items-center justify-center min-h-screen p-4">
+          <Toaster/>
+          {children}
+        </main>
       </PageContextProvider>
     </React.StrictMode>
   )
--- src/pages/index/index.page.tsx
+++ src/pages/index/index.page.tsx
@@ -1,5 +1,4 @@
 import { type ChangeEvent, useState, useEffect } from 'react'
-import { Toaster } from 'react-hot-toast'
 import { useSearch } from '@/hooks/useSearch'
 import { ResultsContainer } from '@/components/resultsContainer'
 
@@ -9,8 +8,7 @@ type Result = Omit<User, 'login'> & {
 
 export const Page = () => {
   const [search, setSearch] = useState('')
-  const { handleSearch } = useSearch()
-  const [barFocused, setBarFocused] = useState(false)
+  const { handleSearch, results } = useSearch()
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     setSearch(e.target.value)
@@ -27,32 +25,26 @@ export const Page = () => {
   }, [search])
 
   return (
-    <main className="flex flex-col items-center justify-center min-h-screen p-4">
-      <Toaster/>
-      <div className={barFocused ? 'w-full -translate-y-40 transition-transform duration-200 ease-in-out' : 'w-full transition-transform duration-200 ease-in-out'}>
-        <h2 className="mx-auto w-fit font-bold text-6xl mb-10">GitHub Search</h2>
-        <div className="flex items-center rounded-lg bg-zinc-200 max-w-3xl sm:max-w-4/6 mx-auto overflow-hidden">
-          <button className="px-4 py-2 bg-zinc-300 hover:bg-zinc-400 transition-colors duration-300 ease-in-out text-neutral-900">
-            Search
-          </button>
-          <input
-            className="bg-transparent w-full p-2 text-neutral-900"
-            placeholder="Search in GitHub"
-            autoComplete="off"
-            type="text"
-            value={search}
-            onChange={handleChange}
-            onFocus={() => setBarFocused(true)}
-            onBlur={() => setBarFocused(false)}
-          />
-        </div>
-        <div className="relative max-w-3xl sm:max-w-4/6 mx-auto">
-          <ResultsContainer
-            results={results as unknown as Result[]}
-            barFocused={barFocused}
-          />
-        </div>
+    <div className={results.length > 0 ? 'w-full -translate-y-40 transition-transform duration-200 ease-in-out' : 'w-full transition-transform duration-200 ease-in-out'}>
+      <h2 className="mx-auto w-fit font-bold text-6xl mb-10">GitHub Search</h2>
+      <div className="flex items-center rounded-lg bg-zinc-200 max-w-3xl sm:max-w-4/6 mx-auto overflow-hidden">
+        <button className="px-4 py-2 bg-zinc-300 hover:bg-zinc-400 transition-colors duration-300 ease-in-out text-neutral-900">
+          Search
+        </button>
+        <input
+          className="bg-transparent w-full p-2 text-neutral-900"
+          placeholder="Search in GitHub"
+          autoComplete="off"
+          type="text"
+          value={search}
+          onChange={handleChange}
+        />
       </div>
-    </main>
+      <div className="relative max-w-3xl sm:max-w-4/6 mx-auto">
+        <ResultsContainer
+          results={results as unknown as Result[]}
+        />
+      </div>
+    </div>
   )
 }
