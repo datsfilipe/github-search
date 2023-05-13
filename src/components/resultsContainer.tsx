c731fa7656466b466721b1c95b7d7f6d8a79c646
datsfilipe <datsfilipe.foss@proton.me>
2023-05-12 22:36:54 -0300

    refactor: resultsContainer component to open links
    It opens the links in a new tab, and it also adds a target="_blank" to the links. If it's a repo it opens on github, if it's a user it opens on /user page.

--- src/components/resultsContainer.tsx
+++ src/components/resultsContainer.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useRef, useState } from 'react'
+import { Link } from './Link'
 
 type Result = Omit<User, 'login'> & {
   name: string
@@ -6,33 +7,45 @@ type Result = Omit<User, 'login'> & {
 
 type ResultsContainerProps = {
   results: Result[]
-  barFocused: boolean
 }
 
 const Result = ({ result }: { result: Result }) => {
   const isRepository = result.html_url.split('/').length === 5
 
+  if (isRepository) {
+    return (
+      <a
+        className='flex items-center justify-between px-4 py-2 border-b color-zinc-950 border-zinc-300 hover:bg-zinc-300 transition-colors duration-300 ease-in-out overflow-hidden'
+        href={result.html_url}
+      >
+        <p>{result.name}</p>
+        <p className='text-gray-600'>[repository]</p>
+      </a>
+    )
+  }
+
   return (
-    <div
+    <Link
       className='flex items-center justify-between px-4 py-2 border-b color-zinc-950 border-zinc-300 hover:bg-zinc-300 transition-colors duration-300 ease-in-out overflow-hidden'
+      href={`/user/${result.id}`}
     >
       <p>{result.name}</p>
-      <p className='text-gray-600'>{isRepository ? '[repository]' : '[user]'}</p>
-    </div>
+      <p className='text-gray-600'>[user]</p>
+    </Link>
   )
 }
 
-export const ResultsContainer = ({ results, barFocused }: ResultsContainerProps) => {
+export const ResultsContainer = ({ results }: ResultsContainerProps) => {
   const resultsContainerRef = useRef<HTMLDivElement>(null)
   const [showResults, setShowResults] = useState<boolean>(false)
 
   useEffect(() => {
-    if (results.length > 0 && barFocused) {
+    if (results.length > 0) {
       setShowResults(true)
     } else {
       setShowResults(false)
     }
-  }, [results, barFocused])
+  }, [results])
 
   return (
     <div
